f066f144336f592da86267e9259444b130f2fc56
Eric Castillo <ecast4@gmail.com>
2015-12-02 15:22:32 -0600

    File
    All the files needed to make the website

---
+++ LeagueOfLegends_Selector/LeagueOfLegends_Selector/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Xsl;
+using System.Xml.XPath;
+
+namespace LeagueOfLegends_Selector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Please Put LeagueofLegendsxml.xml in the bin folder with the project.
+            XmlTextReader reader = new XmlTextReader("LeagueofLegendsxml.xml");
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        Console.Write("<" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                    case XmlNodeType.Text:
+                        Console.WriteLine(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement:
+                        Console.Write("</" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                }
+                Console.ReadLine();
+            }
+
+            
+        }
+    }
+}
